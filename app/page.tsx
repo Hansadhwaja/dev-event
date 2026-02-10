22e5c91fd734b2bf4579b53519c54932ba616278
Hansadhwaja <hansadhwajabiswal@gmail.com>
2026-02-10 21:13:38 +0530

    Added Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import EventCard from '@/components/EventCard'
+import ExploreBtn from '@/components/ExploreBtn'
+import { events } from '@/lib/constants'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section>
+      <h1 className='text-center'>The Hub for Every Dev <br /> Event You Can&apos;t Miss</h1>
+      <p className='text-center mt-5'>Hackathons, Meetups, and Conferences, All in One Place</p>
+      <ExploreBtn />
+      <div className='mt-20 space-y-7'>
+        <h3>Featured Events</h3>
+        <ul className='events'>
+          {events.map(event => (
+            <EventCard key={event.slug} {...event} />
+          ))}
+        </ul>
+      </div>
+    </section>
   )
 }
 
---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const ExploreBtn = () => {
+    return (
+        <button type="button" id="explore-btn" className="mt-7 mx-auto">
+            <Link href={"#events"}>
+                Explore Events
+                <Image src={"/icons/arrow-down.svg"} alt="arrow-down" width={24} height={24} />
+            </Link>
+        </button>
+    )
+}
+
+export default ExploreBtn
